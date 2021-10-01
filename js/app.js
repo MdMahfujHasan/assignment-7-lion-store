12fd045515b7d84ad0231dc75a6960336744572f
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2021-10-01 08:26:50 +0600

    added rating and count

--- js/app.js
+++ js/app.js
@@ -5,20 +5,25 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
-    const allProducts = products.map((pd) => pd);
+    const allProducts = products.map(pd => pd);
     for (const product of allProducts) {
         const image = product.images;
         const div = document.createElement("div");
-        div.classList.add("product");
-        div.innerHTML = `<div class="single-product">
-        <div>
-      <img class="product-image" src=${product.image}></img>
+
+        // div.classList.add("product");
+        div.innerHTML = `
+        <div class="single-product">
+          <div>
+             <img class="product-image" src=${product.image}></img>
+          </div>
+             <h3>${product.title}</h3>
+             <p>Category: ${product.category}</p>
+             <h2>Price: $${product.price}</h2>
+             <strong>Rating: ${product.rating.rate}</strong>
+             <p>Total Count: ${product.rating.count}</p>
+             <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to Cart</button>
+          <button id="details-btn" class="btn btn-danger">Details</button>
         </div>
-        <h3>${product.title}</h3>
-        <p>Category: ${product.category}</p>
-        <h2>Price: $ ${product.price}</h2>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button></div>
         `;
         document.getElementById("all-products").appendChild(div);
     }
@@ -31,8 +36,7 @@ const addToCart = (id, price) => {
     updateTaxAndCharge();
     document.getElementById("total-products").innerText = count;
 };
-
-const getInputValue = (id) => {
+const getInputValue = id => {
     const element = document.getElementById(id).innerText;
     const converted = parseFloat(element);
     return converted;
